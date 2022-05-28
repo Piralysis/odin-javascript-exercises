a58ab10fbe91d34113822b7e504c9b0d49793392
Piralysis <Piralysis@users.noreply.github.com>
2022-05-28 01:28:15 -0500

    Completed exercises 1 through 7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(s, n) {
+  let retval = '';
+  if (n < 0) {
+    return 'ERROR';
+  }
+  for (let i = 0; i < n; i++) {
+    retval += s;
+  }
+  return retval;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(n) {
+  let value = (n - 32) * (5 / 9);
+  return round(value);
 };
 
-const ctof = function() {
-
+const ctof = function(n) {
+  let value = n * (9 / 5) + 32;
+  return round(value);
 };
+
+const round = function(value, precision = 1) {
+  let multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
